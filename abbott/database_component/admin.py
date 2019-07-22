5d009467724ecd41eaa5a7852d7abcda61e29114
dosimulation <xiao.chen@ucla.edu>
2019-07-22 10:19:03 -0700

    up to step 5

--- abbott/database_component/admin.py
+++ abbott/database_component/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TransLog
+from .models import TransSurvey
+admin.site.register(TransLog)
+admin.site.register(TransSurvey)
