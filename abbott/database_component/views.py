9e6f8e9a78634c49b99b1d315d0c3c4514bb6720
dosimulation <xiao.chen@ucla.edu>
2019-07-30 12:19:23 -0700

    Adding plot component

--- abbott/database_component/views.py
+++ abbott/database_component/views.py
@@ -1,39 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from .models import *
 from django.core.files.base import ContentFile
-from django.utils.crypto import get_random_string
+# for plotting
+import plotly.offline as opy
+import plotly.graph_objs as go
 
 
-# Create your views here.
+# main page
 def index(request):
     return render(request, 'index.html')
 
+# taking the translog data table, returns the total number
+# of observations back to the transportation html page
+
+# https://stackoverflow.com/questions/5631247/displaying-graphs-charts-in-django
 def transportation(request):
-    return render(request, 'transportation.html')
+
+    Nrows = TransLog.objects.count()
+
+    x = [-2,0,4,6,7]
+    y = [q**2-q+3 for q in x]
+    trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': 10},
+                        mode="lines",  name='1st Trace')
+
+    data=go.Data([trace1])
+    layout=go.Layout(title="Meine Daten", xaxis={'title':'x1'}, yaxis={'title':'x2'})
+    figure=go.Figure(data=data,layout=layout)
+    div = opy.plot(figure, auto_open=False, output_type='div')
+
+    context = {
+        'nobs' : Nrows,
+        'graph': div,
+              }
+    return render(request, 'transportation.html', context)
 
 def diabetes(request):
     return render(request, 'diabetes.html')
 
 def nutrition(request):
     return render(request, 'nutrition.html')
-
-
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    return render(request, 'login.html')
-
-def login_submit_view(request):
-    user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-    if user is not None:
-        login(request, user)
-        return redirect('/')
-    return render(request, 'login.html', {
-        'error_message': "Failed to authenticate.",
-    })
-
-def logout_view(request):
-    logout(request)
-    return redirect('/login/')
